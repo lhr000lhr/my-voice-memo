1ff236097f6d42d9d828621bca39b6c993d6e664
Peter.Li <peter.li@pushpay.com>
2021-09-18 20:51:01 +1200

    feat: 🎸 import expo-av

---
+++ App.jsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { Audio } from 'expo-av';
+
+export default function App() {
+  const [recording, setRecording] = React.useState();
+
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI();
+    console.log('Recording stopped and stored at', uri);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button
+        title={recording ? 'Stop Recording' : 'Start Recording'}
+        onPress={recording ? stopRecording : startRecording}
+      />
+    </View>
+  );
+
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Mobile Engineer Test.md
@@ -0,0 +1,32 @@
+# Oscer Engineer Team Interview - Project Test 
+Mobile Engineer
+ 
+## Intro
+
+This test asks you to develop a simple mobile App. You would have 1 week to finish it.
+ 
+
+## Requirements
+
+You are required to build an App with voice recording feature which allows users to record their voice. 
+
+And it must support the following user stories:
+
+1. As a user, I can record voice by pressing the `start` button
+2. As a user, I can pause the recording by pressing `pause` button
+3. As a user, I can stop recording by pressing `stop` button
+4. Bonus - As a user, I can have my voice conveted to text
+
+### Requirements List
+
+1. Build up the proper testing components.
+2. Implement a bug tracking/reporting mechanism.
+3. Build up CI/CD pipeline for automatically running tests,
+4. Record a demo video to present the App.
+
+## Submission
+
+Put all your work on Github and send the github link back to us. Please include a readme.md to document your work properly.
+
+## Thank you
+Thanks for your hard work. Looking forward to meeting you soon! Please feel free to contact us if you do have any questions
