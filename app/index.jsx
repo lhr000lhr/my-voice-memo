4c1998e6e9e405b1d5fc725a13456d5bde95cf7c
Peter.Li <peter.li@pushpay.com>
2021-09-19 01:16:04 +1200

    feat: 🎸 add dva and redux persist

---
+++ app/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { AsyncStorage } from 'react-native'
+import { persistStore, autoRehydrate } from 'redux-persist'
+import { registerRootComponent } from 'expo'
+
+import dva from './utils/dva'
+import Router from './router'
+import { registerModels } from './models'
+
+const app = dva({
+  initialState: {},
+  models: [],
+  extraEnhancers: [autoRehydrate()],
+  onError(e) {
+    console.log('onError', e)
+  },
+})
+
+registerModels(app)
+
+const App = app.start(<Router />)
+persistStore(app.getStore(), {
+  storage: AsyncStorage,
+  blacklist: ['router'],
+})
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+
+registerRootComponent(App)
