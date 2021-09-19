b938e416d2933c84aa1eebccb1175cc0e7432540
Peter.Li <peter.li@pushpay.com>
2021-09-19 18:41:21 +1200

    feat: 🎸 convert audio to wav format

--- app/components/RecorderPanel.jsx
+++ app/components/RecorderPanel.jsx
@@ -19,7 +19,28 @@ function RecorderPanel({ dispatch }) {
         playsInSilentModeIOS: true,
       })
       console.log('Starting recording..')
-      const player = await Audio.Recording.createAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY)
+      const recordingOptions = {
+        // android not currently in use, but parameters are required
+        android: {
+          extension: '.m4a',
+          outputFormat: Audio.RECORDING_OPTION_ANDROID_OUTPUT_FORMAT_MPEG_4,
+          audioEncoder: Audio.RECORDING_OPTION_ANDROID_AUDIO_ENCODER_AAC,
+          sampleRate: 44100,
+          numberOfChannels: 2,
+          bitRate: 128000,
+        },
+        ios: {
+          extension: '.wav',
+          audioQuality: Audio.RECORDING_OPTION_IOS_AUDIO_QUALITY_HIGH,
+          sampleRate: 44100,
+          numberOfChannels: 1,
+          bitRate: 128000,
+          linearPCMBitDepth: 16,
+          linearPCMIsBigEndian: false,
+          linearPCMIsFloat: false,
+        },
+      }
+      const player = await Audio.Recording.createAsync(recordingOptions)
       setRecording(player.recording)
       console.log('Recording started')
     } catch (err) {
