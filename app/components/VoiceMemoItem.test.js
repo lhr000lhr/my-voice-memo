a0159221f9419c17e25339ea82707f0ecd7604c5
Peter.Li <peter.li@pushpay.com>
2021-09-19 22:36:09 +1200

    test: 💍 mock momentjs now

--- app/components/VoiceMemoItem.test.js
+++ app/components/VoiceMemoItem.test.js
@@ -2,6 +2,8 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import VoiceMemoItem from './VoiceMemoItem'
 
+jest.mock('moment', () => () => jest.requireActual('moment')('2020-01-01T00:00:00.000Z'))
+
 describe('VoiceMemoItem', () => {
   it('renders correctly with VoiceMemoItem', () => {
     expect(renderer.create(<VoiceMemoItem />)).toMatchSnapshot()
